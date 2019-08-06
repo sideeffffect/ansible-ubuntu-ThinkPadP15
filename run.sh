21bc4b4345f8dc2ba9579806746c33529329b1ef
Ondra Pelech <ondrej.pelech@avast.com>
2019-08-06 22:03:23 +0200

    run.sh parameters

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 ansible-galaxy install --roles-path roles -r requirements.yml && \
-ansible-playbook -i hosts playbook.yml
+ansible-playbook -i hosts playbook.yml "$@"
 
